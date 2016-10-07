3d79b97a3c0791453a0b844e3f617730ddf44227
FawazFarid <fawwazally@gmail.com>
2016-10-07 16:40:28 +0300

    Optimize using Asymptotic Analysis

--- prime.py
+++ prime.py
@@ -1,17 +1,4 @@
-# def prime(num):
-# 	for n in range(0, num+1):
-# 	    if all(n%i!=0 for i in range(2,n)):
-# 	    	print n
-# prime(10)
-# 
-
 def prime(num):
-	for n in range(2, num+1):
-		prime = True
-		for i in range(2, n):
-			if n%i==0:
-				prime=False
-		if prime:
-			print n;
-
-prime(10)
+	for n in range(0, num+1):
+	    if all(n%i!=0 for i in range(2,n)):
+	    	print n
